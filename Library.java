43b44d1a86791303dea5c28640510c2a5886e4f4
Christopher Herrera <cherrera@is4tech.com>
2024-08-24 23:28:48 -0600

    Main and Library class were created

--- Library.java
+++ Library.java
@@ -22,6 +22,9 @@ public class Library {
         for (Libro book : books) {
             if (book.getNombre().equals(name)) {
                 System.out.println("Book found: " + book.getNombre());
+                System.out.println("Author:".concat(book.getAutor().getNombre()));
+                System.out.println("Age: ".concat(String.valueOf(book.getAutor().getEdad())));
+                System.out.println("Category: ".concat(book.getCategoria()));
                 wasFound = true;
                 break;
             }
@@ -29,6 +32,7 @@ public class Library {
         if (!wasFound) {
             System.out.println("Book not found");
         }
+        System.out.println("");
     }
 
 }
--- Main.java
+++ Main.java
@@ -4,8 +4,15 @@ public class Main {
     public static void main(String[] args)
     {
         var library = new Library();
+        var author = new Autor();
+        author.setNombre("J.K. Rowling");
+        author.setEdad(15);
+
         var book = new Libro();
         book.setNombre("Harry Potter");
+        book.setCategoria("Fantasy");
+        book.setAutor(author);
+
         library.addBook(book);
         library.searchBook("Harry Potter");
         library.removeBook(book);
